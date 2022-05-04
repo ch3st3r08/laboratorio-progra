63b243f4b62574be1678f93a95373b866145a6e3
Nestor Chavez <nestor_chavez@hotmail.es>
2022-05-04 15:48:44 -0600

    Usando Axios para consumir api de bitcoin

--- bitcoin-price.js
+++ bitcoin-price.js
@@ -1,1 +1,19 @@
 //Empezar
+
+const axios =  require("axios")
+const fs = require("fs").promises;
+
+const urlData = "https://api.coindesk.com/v1/bpi/currentprice.json";
+let bitcoinData = "";
+
+axios.get(urlData)
+    .then(response => {
+        Object.entries(response.data.bpi).forEach(currency =>{
+            bitcoinData += `${currency[1].description}; ${currency[1].code}; ${currency[1].rate}\n`
+        })
+        //console.log(bitcoinData)
+        return fs.writeFile("datos_bitcoin.csv", bitcoinData)
+    })
+    .then(()=>{
+        console.log("Archivo generado con exito")
+    })
